20ffbb5d7b8d965638da6e6272da9140ddcfc8f1
Sam Hellawell <sshellawell@gmail.com>
2020-02-07 20:08:18 +0000

    add vote type, tally type and use title/desc fields in submit proposal form

--- pages/submit-proposal.js
+++ pages/submit-proposal.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 import styled from 'styled-components';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
@@ -7,13 +7,45 @@ import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 
 import substrateService from '../services/substrate';
 
+// TODO: finish ui for submtiting and selecting various options
+
+const voteTypes = [
+  'Binary',
+  'MultiOption',
+  'RankedChoice'
+];
+
+const tallyTypes = [
+  'OnePerson',
+  'OneCoin'
+];
+
 export default () => {
   const [hash, setHash] = useState();
+  const [state, setState] = useState({
+    voteType: 0,
+    tallyType: 0,
+  });
+  const inputLabel = useRef(null);
   const handleHashChange = event => {
     setHash(event.target.value);
+  };
+
+  const handleChange = event => {
+    const newState = {};
+    newState[event.target.name] = event.target.value;
+    setState({
+      ...state,
+      ...newState,
+    });
   };
 
   function handleAdvanceProposal(e) {
@@ -24,20 +56,16 @@ export default () => {
   function handleCreateProposal(e) {
     e.preventDefault();
 
-    const title = 'My Proposal';
-    const contents = JSON.stringify({
-      description: 'test'
-    });
-
     // fixed size 32 length array of u8
     const YES_VOTE = new Uint8Array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]);
     const NO_VOTE = new Uint8Array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
-
     const outcomes = [YES_VOTE, NO_VOTE];
-    const voteType = 0; // Binary, MultiOption, RankedChoice
-    const tallyType = 0; // OnePerson, OneCoin
 
-    substrateService.createProposal(title, contents, outcomes, voteType, tallyType);
+    const contents = JSON.stringify({
+      description: state.description
+    });
+
+    substrateService.createProposal(state.title, contents, outcomes, state.voteType, state.tallyType);
   }
 
   return (
@@ -51,15 +79,52 @@ export default () => {
           name="title"
           label="Title"
           variant="outlined"
+          onChange={handleChange}
           fullWidth />
         <br /><br />
         <TextField
           name="description"
           label="Description"
           variant="outlined"
+          onChange={handleChange}
           fullWidth
           multiline />
         <br /><br />
+
+        <FormControl variant="outlined" fullWidth>
+          <InputLabel ref={inputLabel}>
+            Vote Type
+          </InputLabel>
+          <Select
+            name="voteType"
+            value={state.voteType}
+            onChange={handleChange}
+          >
+            {voteTypes.map((type, index) => (
+              <MenuItem value={index} key={index}>{type}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <br /><br />
+
+        <FormControl variant="outlined" fullWidth>
+          <InputLabel ref={inputLabel}>
+            Vote Type
+          </InputLabel>
+          <Select
+            name="tallyType"
+            value={state.tallyType}
+            onChange={handleChange}
+          >
+            {tallyTypes.map((type, index) => (
+              <MenuItem value={index} key={index}>{type}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+
+        <br /><br />
+
         <Button
           variant="contained"
           color="primary"
