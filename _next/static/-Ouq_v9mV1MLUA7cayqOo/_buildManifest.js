fb0006e923fc827b5b12a505b9a0f1eecefe7239
Sam Hellawell <sshellawell@gmail.com>
2020-02-14 19:20:57 +0000

    Updates

---
+++ _next/static/-Ouq_v9mV1MLUA7cayqOo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/advance-proposal":[a,b],"/proposal/[id]":[a],"/submit-proposal":[a,b]}}("static\u002Fchunks\u002F82b9ddb221c226ff78a1158315b40da0c5f76549.0e7c7efa6d95fcced560.js","static\u002Fchunks\u002Ff11a9131cce821d3b1e9f9def292dbc3451386f7.f5b17285832e251a64d7.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
