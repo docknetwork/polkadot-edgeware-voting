db8405bc87cb6b68242b98144d51eac401d0d742
Sam Hellawell <sshellawell@gmail.com>
2020-02-14 20:42:58 +0000

    add basePath to next config

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
   assetPrefix: debug ? '' : '/polkadot-edgeware-voting/',
+  experimental: {
+    basePath: '/polkadot-edgeware-voting',
+  },
 };
